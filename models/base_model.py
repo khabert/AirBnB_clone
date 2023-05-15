2aea6c6a065821e046879e52e39ed96a986dcf73
khabert <khabertzion11@gmail.com>
2023-05-15 07:25:29 -0700

    changes in the console

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,56 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""This script is the base model"""
 
-""" contains a classs BaseModel defining all common attributes and methods
-"""
-
-from datetime import datetime
-import models
 import uuid
-import storage
+from datetime import datetime
+from models import storage
+
 
 class BaseModel:
-    """base class from which other subclasses derive"""
+
+    """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-        """initialisation"""
-        if Kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type (self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
-            model.storage.save()
+            storage.new(self)
 
     def __str__(self):
-        """Base class string represeantation"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__,self.id, self.__dict__)
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
-        newdict = self.__dict__.copy()
-        newdict["__class__"] = self.__class__.__name__
-        newdict["created_at"] = self.created_at.isoformat()
-        newdict["created_at"] = self.updated_at.isoformat()
+        """returns a dictionary containing all keys/values of __dict__"""
 
-        return newdict
-
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
